a8b580920d78d89cd067b19f7d8a36689a01af63
kumudgarg <kumud50garg@gmail.com>
2019-11-15 17:44:39 +0530

    To stop the game at winning position 100

--- SnakeAndLadderSimulation.sh
+++ SnakeAndLadderSimulation.sh
@@ -1,22 +1,43 @@
 #!/bin/bash -x
-player=1
+PLAYER=1
 position=0
-snake=1
-ladder=0
-noPlay=2
-dice=$(( RANDOM%6 + 1 ))
-checkOptions=$(( RANDOM%3 ))
+WINNING_POSITION=100
+DEFAULT_POSITION=0
+SNAKE=2
+LADDER=1
+NO_PLAY=0
 function playGame()
-{
-	case $checkOptions in
-	$sanke)
-		position=$(( $position - $dice ));;
-	$ladder)
-		position=$(( $position + $dice ));;
-	$noplay)
-		position=$position;;
-	esac
+{ 
+	
+
+	while [ $position -lt $WINNING_POSITION ]
+	do
+		dice=$(( RANDOM%6 + 1 ))
+		checkOptions=$(( RANDOM%3 ))
+		case $checkOptions in
+		$SNAKE)
+			position=$(( position - dice ));;
+		$LADDER)
+			position=$(( position + dice ));;
+		$NO_PLAY)
+			position=$position;;
+		esac
+			reachToWinningPosition
+	done
 }
+function reachToWinningPosition()
+{
+	if [ $position -lt $DEFAULT_POSITION ]
+   then
+		position=$DEFAULT_POSITION
+	elif [ $position -gt $WINNING_POSITION ]
+	then
+		 position=$WINNING_POSITION
+	else
+		echo "be continued"
+	fi
+}
+
 playGame
 echo "welcome to snake and Ladder"
 echo $player $position
