bbf8e6e278439c4599598285cf5e3fd6c2e10924
SandhyaBridgelabz <sandhyapatil364@gmail.com>
2019-11-15 12:34:48 +0530

    in function play game check case that player got either sanke or dice or no play

--- SnakeAndLadderSimulation.sh
+++ SnakeAndLadderSimulation.sh
@@ -1,7 +1,25 @@
 #!/bin/bash -x
 player=1
 position=0
-dice=$(( RANDOM%6 + 1 )) 
+snake=1
+ladder=0
+noPlay=2
+dice=$(( RANDOM%6 + 1 ))
+checkOptions=$(( RANDOM%3 ))
+function playGame()
+{
+	case $checkOptions in
+	$sanke)
+		position=$(( $position - $dice ));;
+	$ladder)
+		position=$(( $position + $dice ));;
+	$noplay)
+		position=$position;;
+	esac
+}
+playGame
 echo "welcome to snake and Ladder"
 echo $player $position
 echo $dice
+echo $position
+
