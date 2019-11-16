c094f83e71697a8303e2753a63c5d5630d2346df
kumudgarg <kumud50garg@gmail.com>
2019-11-16 10:40:23 +0530

    Conclusion:which player won

--- SnakeAndLadderSimulation.sh
+++ SnakeAndLadderSimulation.sh
@@ -1,17 +1,22 @@
 #!/bin/bash -x
+
+#<-----Constants-------------->
 PLAYER=1
-position=0
 WINNING_POSITION=100
 DEFAULT_POSITION=0
 SNAKE=2
 LADDER=1
 NO_PLAY=0
+#<-----Global variables-------->
+position=0
 count=0
+counter=0
+playerPosition1=0
+playerPosition2=0
 declare -A playerResultInfo
 function playGame()
 { 
 	
-
 	while [ $position -lt $WINNING_POSITION ]
 	do
 		dice
@@ -23,36 +28,49 @@ function playGame()
 		$NO_PLAY)
 			position=$position;;
 		esac
+			swichPlayer
 			reachToWinningPosition
+			wonPlayer
 	done
 }
 function reachToWinningPosition()
 {
 	playerResultInfo[$count]=$position
-        count=$(( $count + 1 ))
+	count=$(( count+1 ))
 	if [ $position -lt $DEFAULT_POSITION ]
 	then
 		position=$DEFAULT_POSITION
 	elif [ $position -gt $WINNING_POSITION ]
-        then
-                position=$(( position - dice ))
-		
-
-	else		
-		echo "be continued"
+   then
+      position=$(( position - dice ))
 	fi
-
 }
 function dice()
 {
-	 dice=$(( RANDOM%6 + 1 ))
-         checkOptions=$(( RANDOM%3 ))
+	dice=$(( RANDOM%6 + 1 ))
+   checkOptions=$(( RANDOM%3 ))
+}
+function wonPlayer()
+{
+	if [ $playerPosition1 -eq $WINNING_POSITION ]
+	then
+		echo "player 1 win "
+	elif [ $playerPosition2 -eq $WINNING_POSITION ]
+	then
+		echo "player 2 win "
+	fi
+}
+function swichPlayer()
+{
+  	if [ $(( counter%2 )) -eq 0 ]
+	then
+		playerPosition1=$position
+	else
+		playerPosition2=$position
+	fi 
+		counter=$(( counter + 1 ))
 }
 playGame
 echo "welcome to snake and Ladder"
-echo $player $position
-echo $dice
-echo $position
-echo ${playerResultInfo[@]}
-echo "to won the game dice was rolled ${#playerResultInfo[@]} times "
- 
+echo "to won the game dice was rolled $count times "
+echo "position occupied by the players are ${playerResultInfo[@]}"
